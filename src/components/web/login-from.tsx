9ccb6500511140abb21088cbadb487d40efa4687
fong <botlorkfong@gmail.com>
2026-01-14 22:29:43 +0700

    initial commit

---
+++ src/components/web/login-from.tsx
@@ -0,0 +1,113 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { Link } from '@tanstack/react-router'
+import { useForm } from '@tanstack/react-form'
+import { loginSchema } from '@/schemas/auth'
+
+export function LoginForm() {
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    validators: {
+      onSubmit: loginSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log('values:', value)
+    },
+  })
+  return (
+    <Card className="max-w-md w-full">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            form.handleSubmit()
+          }}
+        >
+          <FieldGroup>
+            <form.Field
+              name="email"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      type="email"
+                      placeholder="email@example.com"
+                      autoComplete="off"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                )
+              }}
+            />
+            <form.Field
+              name="password"
+              children={(field) => {
+                const isInvalid =
+                  field.state.meta.isTouched && !field.state.meta.isValid
+                return (
+                  <Field data-invalid={isInvalid}>
+                    <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    <Input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      type="password"
+                      placeholder="********"
+                      autoComplete="off"
+                    />
+                    {isInvalid && (
+                      <FieldError errors={field.state.meta.errors} />
+                    )}
+                  </Field>
+                )
+              }}
+            />
+            <Field>
+              <Button type="submit">Login</Button>
+              <FieldDescription className="text-center">
+                Don&apos;t have an account? <Link to="/signup">Sign up</Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/components/web/navbar.tsx
@@ -0,0 +1,32 @@
+import { Link } from '@tanstack/react-router'
+import { buttonVariants } from '../ui/button'
+import { ThemeToggle } from './theme-toggle'
+
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+          <img
+            src="https://tanstack.com/images/logos/logo-color-banner-600.png"
+            alt="TanStack Logo"
+            className="size-12"
+          />
+          <h1 className="text-lg font-semibold">TanStack Start</h1>
+        </div>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <Link
+            className={buttonVariants({ variant: 'secondary' })}
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link className={buttonVariants()} to="/signup">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
